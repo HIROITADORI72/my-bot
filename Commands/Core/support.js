8946e3e1e32319b4173c5afe4ab7abb6e40289ae
Senpai_Yuji <115178136+HIROITADORI72@users.noreply.github.com>
2023-03-18 00:49:26 +0200

    Update support.js

--- Commands/Core/support.js
+++ Commands/Core/support.js
@@ -8,7 +8,7 @@ module.exports = {
     start: async(Miku, m,{pushName}) => {
         m.reply(`Check your DM *${pushName}* Senpai !\n\nI have sent you support group link personally.`)
         let botpic = botImage1
-        let txt = `      🧣 *Support Group* 🧣\n\n*${botName}* is an open source project, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help in *Group*.\n\n*Thanks for using Atlas.*`
+        let txt = `      ✨️ *Support Group* 🐉\n\n*${botName}* is an open source project, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help in *Group*.\n\n*Thanks for using CELESTIC BOTZ.*`
         await Miku.sendMessage(m.sender,{image:{url:botpic}, caption:txt},{quoted:m})
     }
 }
